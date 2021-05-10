a7c16b030cfc22a1de78a1b5eead890921cf75ce
Usuário <Usurio@192.168.15.5>
2021-05-09 20:31:30 -0400

    Primeira versão

---
+++ Ajudas - Renan/src/Média.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Média {
+
+	public static void main(String[] args) {
+
+		double NM;
+		double N;
+		double soma = 0;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		//NM = Número a ser tirados a média
+		System.out.println("Informe a quantidade de números que deseja tirar a média");
+		NM = entrada.nextInt();
+		
+		for(int x = 1; x <= NM; x++) {
+			System.out.println("Informe o " + x + " Número");
+			N = entrada.nextInt();
+			
+			soma += N ;
+		}
+		System.out.println("Média: " + soma/NM);
+	}
+
+}
